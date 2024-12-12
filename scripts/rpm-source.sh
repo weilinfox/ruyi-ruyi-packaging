be1479e7109f979cd2e4f2655d30340164b6cb2a
weilinfox <caiweilin@iscas.ac.cn>
2024-12-12 16:19:46 +0800

    Install mock
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- scripts/rpm-source.sh
+++ scripts/rpm-source.sh
@@ -2,9 +2,72 @@
 
 set -e
 
-sudo apt-get install dnf
+# install mock
+mkdir mock-source
+cd mock-source
+wget https://github.com/rpm-software-management/mock/releases/download/mock-5.9-1/mock-5.9.tar.gz
+tar -xf mock-5.9.tar.gz
 
-whoami
+sitelib=$(python3 -c 'from sysconfig import get_path; import sys; sys.stdout.write(get_path(name="purelib"))')
+pkgver=5.9
+rpmrel=1
+
+sed -r -i py/mockbuild/constants.py py/mock-parse-buildlog.py \
+	-e 's|^VERSION\s*=.*|VERSION="'$pkgver'"|' \
+	-e 's|^SYSCONFDIR\s*=.*|SYSCONFDIR="/etc"|' \
+	-e 's|^PYTHONDIR\s*=.*|PYTHONDIR="'$sitelib'"|' \
+	-e 's|^PKGPYTHONDIR\s*=.*|PKGPYTHONDIR="'$sitelib'/mockbuild"|'
+sudo mkdir -p /etc/mock/eol/templates
+sudo mkdir -p /etc/mock/templates
+
+sudo install -d /usr/libexec/mock
+sudo install mockchain /usr/bin/mockchain
+sudo install py/mock-hermetic-repo.py /usr/bin/mock-hermetic-repo
+sudo install py/mock-parse-buildlog.py /usr/bin/mock-parse-buildlog
+sudo install py/mock.py /usr/libexec/mock/mock
+sudo ln -s consolehelper /usr/bin/mock
+sudo install create_default_route_in_container.sh /usr/libexec/mock/
+
+# sudo install -d /etc/pam.d
+sudo cp -a etc/pam/* /etc/pam.d/
+
+sudo install -d /etc/mock
+sudo cp -a etc/mock/* /etc/mock/
+
+sudo install -d /etc/security/console.apps/
+sudo cp -a etc/consolehelper/mock /etc/security/console.apps/mock
+
+# sudo install -d /usr/share/bash-completion/completions/
+sudo cp -a etc/bash_completion.d/* /usr/share/bash-completion/completions/
+sudo cp -a mock.complete /usr/share/bash-completion/completions/mock
+sudo ln -s mock /usr/share/bash-completion/completions/mock-parse-buildlog
+
+sudo install -d /etc/pki/mock
+sudo cp -a etc/pki/* /etc/pki/mock/
+
+sudo cp -a py/mockbuild $sitelib/
+
+# sudo install -d /usr/share/man/man1
+sudo cp -a docs/mock.1 docs/mock-parse-buildlog.1 mock-hermetic-repo.1 /usr/share/man/man1/
+sudo install -d /usr/share/cheat
+sudo cp -a docs/mock.cheat /usr/share/cheat/mock
+
+sudo install -d /var/lib/mock
+sudo install -d /var/cache/mock
+
+sudo mkdir -p /usr/share/doc/mock
+install -p -m 0644 docs/buildroot-lock-schema-*.json /usr/share/doc/mock/
+install -p -m 0644 docs/site-defaults.cfg /usr/share/doc/mock
+
+# sudo mkdir -p /usr/lib/sysusers.d
+sudo install -p -D -m 0644 mock.conf /usr/lib/sysusers.d
+
+sudo sed -i 's/^_MOCK_NVR = None$/_MOCK_NVR = "mock-'$pkgver-$rpmrel'"/' \
+    /usr/libexec/mock/mock
+
+sudo mock --version
+
+sudo apt-get install dnf
 
 ls /etc/mock
 
